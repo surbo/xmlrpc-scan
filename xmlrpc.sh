48d1e999a1c1494f77ffc4562652c7395deb1e9d
SuRb0 <1809870+surbo@users.noreply.github.com>
2020-09-21 17:02:18 -0500

    Update xmlrpc.sh
    Added Current Domain Scan and XMLRPC Found

--- xmlrpc.sh
+++ xmlrpc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# By @surbo 2020
-#quick and dirty script to scan a list of domains
-#Example cat list.txt |./xmlrpc.sh
+# By @surbo 9/21/2020
 stamp=$(date +"%m-%d-%Y-%T") #timestamp
+touch XML-RPC_RESPONSE_$stamp.csv
 while read Domains
 do
-xmlcheck=$(curl -s -L $Domains/xmlrpc.php)
+found=$(cat XML-RPC_RESPONSE_$stamp.csv|wc -l)
+printf "                                                                                                              "
+printf "\rCurrently Checking: $Domains Domains with XML-RPC: $found\r"
+xmlcheck=$(curl --connect-timeout 2.37 -s -L $Domains/xmlrpc.php)
 if [[ $xmlcheck = "XML-RPC server accepts POST requests only." ]]
 then
   echo $Domains>>XML-RPC_RESPONSE_$stamp.csv
